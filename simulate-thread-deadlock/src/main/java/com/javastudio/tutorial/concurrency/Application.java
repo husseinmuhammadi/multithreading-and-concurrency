4a1cea1d6be5b1bfe664d73edac78ae968a248f8
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2020-03-31 13:50:20 +0430

    Simulate thread deadlock

--- simulate-thread-deadlock/src/main/java/com/javastudio/tutorial/concurrency/Application.java
+++ simulate-thread-deadlock/src/main/java/com/javastudio/tutorial/concurrency/Application.java
@@ -1,6 +1,11 @@
 package com.javastudio.tutorial.concurrency;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     private static final Object lock1 = new Object();
     private static final Object lock2 = new Object();
@@ -8,18 +13,22 @@ public class Application {
     public static void main(String[] args) {
         Thread thread1 = new Thread(() -> {
             synchronized (lock1) {
-                System.out.println(Thread.currentThread().getName());
+                LOGGER.info("{}: Holding lock 1 ...", Thread.currentThread().getName());
+                LOGGER.info("{}: Waiting for lock 2 ...", Thread.currentThread().getName());
                 sleep(1000);
                 synchronized (lock2) {
+                    LOGGER.info("{}: Holding lock 1 & 2 ...", Thread.currentThread().getName());
                     System.out.println(Thread.currentThread().getName());
                 }
             }
         });
         Thread thread2 = new Thread(() -> {
             synchronized (lock2) {
-                System.out.println(Thread.currentThread().getName());
+                LOGGER.info("{}: Holding lock 2 ...", Thread.currentThread().getName());
+                LOGGER.info("{}: Waiting for lock 1 ...", Thread.currentThread().getName());
                 sleep(1000);
                 synchronized (lock1) {
+                    LOGGER.info("{}: Holding lock 1 & 2 ...", Thread.currentThread().getName());
                     System.out.println(Thread.currentThread().getName());
                 }
             }
