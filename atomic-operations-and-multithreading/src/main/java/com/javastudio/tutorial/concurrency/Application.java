da013ac3dcd3f580c424a42b86a79f0d2e73e799
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-05-30 09:21:28 +0430

    atomic operations

--- atomic-operations-and-multithreading/src/main/java/com/javastudio/tutorial/concurrency/Application.java
+++ atomic-operations-and-multithreading/src/main/java/com/javastudio/tutorial/concurrency/Application.java
@@ -3,6 +3,8 @@ package com.javastudio.tutorial.concurrency;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ObjectInputStream;
+
 public class Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
@@ -15,11 +17,14 @@ public class Application {
         LOGGER.info("Index: {}", index);
     }
 
+    private static final Object lock=new Object();
     private static int index = 0;
 
-    private static void increase() {
-        for (int i = 0; i < 100000000; i++) {
-            index++;
+    private /*synchronized*/ static void increase() {
+        for (int i = 0; i < 1000000000; i++) {
+            // synchronized (lock) {
+                index++;
+            // }
         }
 
         LOGGER.info("Thread {} has finished its activity.", Thread.currentThread().getName());
