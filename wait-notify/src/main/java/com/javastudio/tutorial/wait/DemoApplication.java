fe01c6c11a7e76acea9c75f44649f1fd4706fba0
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-08-04 15:58:08 +0200

    logback.xml

---
+++ wait-notify/src/main/java/com/javastudio/tutorial/wait/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.javastudio.tutorial.wait;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DemoApplication {
+    private final static Logger LOGGER = LoggerFactory.getLogger(DemoApplication.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Application started!");
+    }
+}
